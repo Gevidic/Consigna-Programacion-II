3aeac06e67875f3cb93ae90619b1bc3bfa59095d
Gevidic <183768733+Gevidic@users.noreply.github.com>
2025-04-24 17:23:53 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         AppStore appStore = new AppStore();
-        int opcion = 0;
+        int opcion;
 
         do {
             System.out.println("\n--- MENÚ APPSTORE ---");
@@ -13,12 +13,12 @@ public class Main {
             System.out.println("3. Vender licencias de un juego");
             System.out.println("4. Consultar el juego más vendido");
             System.out.println("5. Calcular descuento por volumen de venta");
-            System.out.println("6. Evaluar compras por política porcentaje (metodo1)");
-            System.out.println("7. Evaluar compras por política categoría (metodo2)");
+            System.out.println("6. Evaluar compras por política porcentaje");
+            System.out.println("7. Evaluar compras por política categoría");
             System.out.println("8. Salir");
-            System.out.print("Elige una opción: ");
+            System.out.print("Opción: ");
             opcion = sc.nextInt();
-            sc.nextLine();
+            sc.nextLine(); // Limpiar buffer
 
             switch (opcion) {
                 case 1:
@@ -29,18 +29,18 @@ public class Main {
                     break;
 
                 case 2:
-                    System.out.print("Nombre del juego: ");
+                    System.out.print("Nombre del juego a comprar: ");
                     String nombreCompra = sc.nextLine();
-                    System.out.print("Cantidad a comprar: ");
+                    System.out.print("Cantidad de licencias: ");
                     int cantidadCompra = sc.nextInt();
                     appStore.comprarLicenciasJuego(nombreCompra, cantidadCompra);
                     System.out.println("Compra realizada.");
                     break;
 
                 case 3:
-                    System.out.print("Nombre del juego: ");
+                    System.out.print("Nombre del juego a vender: ");
                     String nombreVenta = sc.nextLine();
-                    System.out.print("Cantidad a vender: ");
+                    System.out.print("Cantidad de licencias: ");
                     int cantidadVenta = sc.nextInt();
                     boolean vendido = appStore.venderLicenciasJuego(nombreVenta, cantidadVenta);
                     System.out.println(vendido ? "Venta realizada." : "No se pudo realizar la venta.");
@@ -51,10 +51,9 @@ public class Main {
                     break;
 
                 case 5:
-                    System.out.print("Cantidad de licencias a vender para cada juego (en orden 1-4): ");
+                    System.out.print("Licencias a vender (juego1, juego2, juego3, juego4): ");
                     int c1 = sc.nextInt(), c2 = sc.nextInt(), c3 = sc.nextInt(), c4 = sc.nextInt();
-                    String descuento = appStore.calcularVentaPorVolumen(c1, c2, c3, c4);
-                    System.out.println(descuento);
+                    System.out.println(appStore.calcularVentaPorVolumen(c1, c2, c3, c4));
                     break;
 
                 case 6:
@@ -66,7 +65,7 @@ public class Main {
                     break;
 
                 case 8:
-                    System.out.println("Saliendo...");
+                    System.out.println("¡Gracias por usar la AppStore!");
                     break;
 
                 default:
@@ -78,9 +77,10 @@ public class Main {
     }
 
     private static void mostrarInfoJuego(Juego juego) {
-        System.out.println("\nNombre: " + juego.darNombre());
+        System.out.println("\n--- Información del Juego ---");
+        System.out.println("Nombre: " + juego.darNombre());
         System.out.println("Categoría: " + juego.darCategoria());
-        System.out.println("Precio: $" + juego.darPrecio());
+        System.out.println("Precio: $" + juego.darPrecio() + " USD");
         System.out.println("Tamaño: " + juego.darTamanio() + " KB");
         System.out.println("Licencias disponibles: " + juego.darCantidadLicencias());
         System.out.println("Licencias vendidas: " + juego.darCantidadVendidas());
